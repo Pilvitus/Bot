72b65c617304625d8543541fd8af21552665baf2
Pilvitus <65862746+Pilvitus@users.noreply.github.com>
2024-10-01 23:50:07 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from flask import Flask, request
 app = Flask(__name__)
 bot = telebot.TeleBot(os.environ['TELEGRAM_TOKEN'])
 
+# Устанавливаем маршрут для обработки сообщений
 @app.route('/' + os.environ['TELEGRAM_TOKEN'], methods=['POST'])
 def get_message():
     json_str = request.get_json(force=True)
@@ -12,11 +13,13 @@ def get_message():
     bot.process_new_updates([update])
     return '!', 200
 
+# Основной маршрут для проверки работы вебхука
 @app.route('/')
-def webhook():
-    return 'Webhook is set up!', 200
+def index():
+    return 'Webhook is working!', 200
 
+# Устанавливаем вебхук
 if __name__ == '__main__':
     bot.remove_webhook()
-    bot.set_webhook(url='https://<your-app-name>.railway.app/' + os.environ['TELEGRAM_TOKEN'])
+    bot.set_webhook(url=f'https://Bot.railway.app/{os.environ["TELEGRAM_TOKEN"]}')
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
