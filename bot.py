fb991930dee1c3a7e7071ec4503dc2ae1c7d8279
Pilvitus <65862746+Pilvitus@users.noreply.github.com>
2024-10-01 21:29:45 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ import telebot
 from flask import Flask, request
 
 app = Flask(__name__)
-bot = telebot.TeleBot(os.environ['7876725841:AAH_XrZvJeiyqPtOwmfGZNIoacG7ZaI8W24'])
+bot = telebot.TeleBot(os.environ['TELEGRAM_TOKEN'])
 
-@app.route('/' + os.environ['7876725841:AAH_XrZvJeiyqPtOwmfGZNIoacG7ZaI8W24'], methods=['POST'])
+@app.route('/' + os.environ['TELEGRAM_TOKEN'], methods=['POST'])
 def get_message():
     json_str = request.get_json(force=True)
     update = telebot.types.Update.de_json(json_str)
@@ -18,5 +18,5 @@ def webhook():
 
 if __name__ == '__main__':
     bot.remove_webhook()
-    bot.set_webhook(url='https://<your-app-name>.railway.app/' + os.environ['7876725841:AAH_XrZvJeiyqPtOwmfGZNIoacG7ZaI8W24'])
+    bot.set_webhook(url='https://<your-app-name>.railway.app/' + os.environ['TELEGRAM_TOKEN'])
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
