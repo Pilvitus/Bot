7b3d5e69e7da840ff8654f3c065dd737e660c9b3
Pilvitus <65862746+Pilvitus@users.noreply.github.com>
2024-10-02 20:00:25 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -5,18 +5,6 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 async def start(update: Update, context):
     await update.message.reply_text(f'Привіт, {update.effective_user.first_name}, цей бот створенно для навчання з математики!')
 
-# Обрабатываем текстовые сообщения
-async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text.lower()
-    
-    if "привіт" in text:
-        await update.message.reply_text("Привіт! Як я можу допомогти тобі з математикою?")
-    elif "формула" in text:
-        await update.message.reply_text("Можу підсказати формули по алгебрі та геометрії. Запитуй!")
-    else:
-        await update.message.reply_text("Я поки не знаю цього, але я вчусь :)")
-
-
 # Основная функция для запуска бота
 if __name__ == '__main__':
     # Вставьте ваш токен ниже
